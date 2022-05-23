fcd6a0cce3b1acb2a711127321e4a9ae2447e3ed
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-05-23 20:15:57 -0300

    Elegimos transistor para el buck

--- calculos_transistor_buck.m
+++ calculos_transistor_buck.m
@@ -7,17 +7,40 @@ Vdrv = Vds % V
 Vo = 6.35
 D = Vo/Vds
 
-% IRF9520
-Rdson = 0.6 % Ohm
-Qgate = 6.89855e-008 % C
-Vth = 4
-tsw = 24e-9 % s
+% % IRF9520
+% Rdson = 0.6 % Ohm
+% Qgate = 6.89855e-008 % C
+% Vth = 4
+% tsw = 24e-9 % s
+% 
+% 
+% 
+% 
+% Pgate = 2 * fsw * Qgate * Vdrv;
+% Pcond = Id.^2 * Rdson * D + Id.^2 * Rdson * 1-D;
+% Psw = 2 * 0.5 * Vds * Id * fsw * tsw;
+% 
+% %Ploss = Pcond + Psw .+ Pgate % Octave
+% Ploss = Pcond + Psw + Pgate; % Matlab
+% figure()
+% plot(Id, Ploss)
+% 
+% eff = (Vds*Id)./(Ploss+(Vds*Id));
+% 
+% figure()
+% plot(Id, eff)
+Vdrv = 10
+% IRF520
+Rdson = 0.23 % Ohm
+Qgate = 7.44063e-009 % C
+Vth = 2.9
+tsw = 16.5e-9 % s
 
 
 
 
 Pgate = 2 * fsw * Qgate * Vdrv;
-Pcond = Id.^2 * Rdson * D + Id.^2 * Rdson * 1-D;
+Pcond = Id.^2 * Rdson
 Psw = 2 * 0.5 * Vds * Id * fsw * tsw;
 
 %Ploss = Pcond + Psw .+ Pgate % Octave
