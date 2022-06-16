77aa4cb3afd81564983a5a86045e4e2d08aec9f5
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-06-15 23:55:05 -0300

    .

---
+++ calculos_inductor_capacitor.m
@@ -0,0 +1,18 @@
+clear all; close all;
+Vs = 6.35 % V
+Ve = 36 % V
+D = Vs / Ve
+f = 200e3 % Hz
+Is = 1% A
+ripple_max_I = 40/100 % 40%
+ILmax = Is * (1 + ripple_max_I/2) % A
+L = 1/2/f * (1-D) * Vs / (ILmax - Is) % H
+
+
+Is = 1.6 % A
+ripple_max_V = 5/100 %
+ILmax = Is + 1/2/f * (1-D) * Vs / L % H
+ILmin = ILmax - Vs* (1-D) /f / L
+C = (ILmax - ILmin)/(16 * ripple_max_V * Vs * f)
+
+
