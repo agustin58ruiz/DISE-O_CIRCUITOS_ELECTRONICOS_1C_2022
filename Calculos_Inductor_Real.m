834293d3ed65b0ab47170cccb8a073bf3543ef86
Agustín Ruiz <aeruiz@fi.uba.ar>
2022-07-01 07:09:42 -0300

    Se agregaron los cálculos para el inductor capacitor

---
+++ Calculos_Inductor_Real.m
@@ -0,0 +1,10 @@
+clear all; close all; clc;
+
+AreaCore =15 ; % En mm2
+AreaWindow = 15; % In mm2
+MeanLengthTurn = 50;
+Resistivity = 1.72e-8;% Ohm x meter
+N = 15; % Wire turns
+Bmax = 15; %Magnetic Flux Density in Tesla
+Mu_0 = 4*pi*e-7; % Magnetic Permeability of free space
+R_gap = 456; % Gap reluctance
